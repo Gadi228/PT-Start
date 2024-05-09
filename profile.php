1b59dc6e66ff2310712836932911ee8a4cd90cdc
Gadi228 <gadji058@bk.ru>
2024-05-09 17:04:35 -0400

    last

--- profile.php
+++ profile.php
@@ -37,9 +37,10 @@
         </h1></div>
       </div>
       <div class="col-12">
-        <form method="POST" action="profile.php">
-          <input type="text" class="form" name="title" placeholder="Заголовок вашего поста">
-          <textarea name="text" cols="30" rows="10" placeholder="Введите текст вашего поста ..."></textarea>
+        <form method="POST" action="profile.php" enctype="multipart/form-data" name="upload"> 
+          <input type="text" class="form" name="title" placeholder="Заголовок вашего поста"><br>
+          <textarea name="text" cols="30" rows="10" placeholder="Введите текст вашего поста ..."></textarea><br>
+          <input type="file" name="file"><br>
           <button type="submit" class="btn_red" name="submit">Сохранить пост!</button>
         </form>
       </div>
@@ -60,5 +61,20 @@ if (isset($_POST['submit'])) {
   if (!$title || !$main_text) die ("Заполните все поля");
   $sql = "INSERT INTO posts (title, main_text) VALUES ('$title', '$main_text')";
   if (!mysqli_query($link, $sql)) die ("Не удалось добавить пост");
+  if(!empty($_FILES["file"]))
+    {
+        if (((@$_FILES["file"]["type"] == "image/gif") || (@$_FILES["file"]["type"] == "image/jpeg")
+        || (@$_FILES["file"]["type"] == "image/jpg") || (@$_FILES["file"]["type"] == "image/pjpeg")
+        || (@$_FILES["file"]["type"] == "image/x-png") || (@$_FILES["file"]["type"] == "image/png"))
+        && (@$_FILES["file"]["size"] < 102400))
+        {
+            move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
+            echo "Load in:  " . "upload/" . $_FILES["file"]["name"];
+        }
+        else
+        {
+            echo "upload failed!";
+        }
+    }
 }
 ?>
